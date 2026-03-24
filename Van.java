49bb3962733ec2ba9d334cf0a123997e7d5186d6
miyuminimsara09 <nimsarapeiris09@gmail.com>
2026-03-24 11:08:41 +0530

    Update Van.java

--- Van.java
+++ Van.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Lasantha
- */
 public class Van implements Vehicle {
 
     @Override
