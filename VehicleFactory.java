6e37c140ddd2aad74e414c56d7122597d666296f
miyuminimsara09 <nimsarapeiris09@gmail.com>
2026-03-24 11:09:13 +0530

    Update VehicleFactory.java

--- VehicleFactory.java
+++ VehicleFactory.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Lasantha
- */
 public class VehicleFactory {
 
     public static Vehicle createVehicle(String type) {
